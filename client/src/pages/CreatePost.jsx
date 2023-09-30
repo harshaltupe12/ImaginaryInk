f363ba89375f3107787ca540179d48cb1e85a0d8
Harshal Tupe <harshaltupe12@gmail.com>
2023-10-01 00:02:35 +0530

    Creating Create Post

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { preview } from '../assets'
+import { getRandomPrompt } from '../utils'
+import { FormField, Loader } from '../components'
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const handelSubmit = () => {
+
+  }
+
+  const handleChange = (e) =>{
+
+  }
+
+  const handleSurpriseMe = () =>{
+    
+  }
   return (
-    <div>CreatePost</div>
+    <section>
+      <div>
+          <h1 className='font-extrabold text-[#222328] text-[32px]'>Create Images</h1>
+          <p className='mt-2 text-[#666e75] text-[16px] max-w[50px]'>Create imaginative and visually stunning images through ImaginaryINK AI and share them with the community</p>
+      </div>
+
+      <form className='mt-16 max-w-3xl' onSubmit={handelSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+           labelName="Your name"
+           type="text"
+           name="name"
+           placeholder="John Doe"
+           value = {form.name}
+           handleChange = {handleChange}
+          />
+
+          <FormField
+           labelName="Prompt"
+           type="text"
+           name="prompt"
+           placeholder="A comic book cover of a superhero wearing headphones"
+           value = {form.prompt}
+           handleChange = {handleChange}
+           isSurpriseMe
+           handleSurpriseMe = {handleSurpriseMe}
+          />
+        </div>
+      </form>
+    </section>
   )
 }
 
