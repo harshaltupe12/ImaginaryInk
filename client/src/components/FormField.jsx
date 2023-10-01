7ea52c20eced8052ba6a0cea2e2da7aa76ea9ee9
Harshal Tupe <harshaltupe12@gmail.com>
2023-10-01 20:51:01 +0530

    Create Form Field Structure

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -9,8 +9,13 @@ const FormField = ({ labelName, type, name, placeholder, value, handleChange, is
         >
         {labelName}
         </label>
-        
+        {isSurpriseMe && (
+          <button type='button' onClick={handleSurpriseMe} className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black hover:bg-gray-300'>
+            Surprise me
+          </button>
+        )}
       </div>
+      <input type={type} id={name} name={name} placeholder={placeholder} value={value} onChange={handleChange} required className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"/>
     </div>
   )
 }
